0d547b135955e4f6306ffc9f6d2bed5cfdc04d7c
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-16 10:13:19 +0100

    added relevant unit tests to the nginx cookbook

--- Cookbooks/nginx/spec/unit/recipes/default_spec.rb
+++ Cookbooks/nginx/spec/unit/recipes/default_spec.rb
@@ -1,18 +1,22 @@
-#
-# Cookbook:: nginx
-# Spec:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
 describe 'nginx::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+
+
     platform 'ubuntu', '18.04'
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
+    end
+    it 'should install nginx' do
+      expect(chef_run).to install_package "nginx"
+    end
+    it 'enable service nginx' do
+      expect(chef_run).to enable_service "nginx"
+    end
+    it 'run service nginx' do
+      expect(chef_run).to start_service "nginx"
     end
   end
 end
