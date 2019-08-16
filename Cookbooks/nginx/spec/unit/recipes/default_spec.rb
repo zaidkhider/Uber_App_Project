fec908412410a19602e529b03e7a590f3937bf07
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-16 15:36:16 +0100

    added the templates and links to the integration test, unit test and the recipe and then added an attributes folder, the unit test was run and all the examples passed

--- Cookbooks/nginx/spec/unit/recipes/default_spec.rb
+++ Cookbooks/nginx/spec/unit/recipes/default_spec.rb
@@ -3,20 +3,36 @@ require 'spec_helper'
 
 describe 'nginx::default' do
   context 'When all attributes are default, on Ubuntu 18.04' do
-
-
+    
     platform 'ubuntu', '18.04'
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
     it 'should install nginx' do
       expect(chef_run).to install_package "nginx"
     end
+
     it 'enable service nginx' do
       expect(chef_run).to enable_service "nginx"
     end
+
     it 'run service nginx' do
       expect(chef_run).to start_service "nginx"
     end
-  end
+
+    it 'should create a proxy.conf template in /etc/nginx/sites-available' do
+      expect(chef_run).to create_template("/etc/nginx/sites-available/proxy.conf").with_variables(proxy_port: 3000)
+    end
+
+    it 'should create a symlink of proxy.conf from sites-available to sites-enabled' do
+      expect(chef_run).to create_link('/etc/nginx/sites-enabled/proxy.conf').with_link_type(:symbolic)
+    end
+
+    it 'runs apt get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+end
 end
