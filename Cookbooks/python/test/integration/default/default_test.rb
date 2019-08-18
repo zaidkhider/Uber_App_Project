5a7cc2b56362418159595c610b5d5c2b90876b66
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-18 22:58:08 +0100

    added the integration test to the python cookbook and also added the the necessary code to the recipe

--- Cookbooks/python/test/integration/default/default_test.rb
+++ Cookbooks/python/test/integration/default/default_test.rb
@@ -1,16 +1,7 @@
-# InSpec test for recipe python::default
-
-# The InSpec reference, with examples and extensive documentation, can be
-# found at https://www.inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package('python') do
+  it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package('python-pip') do
+  it { should be_installed }
 end
