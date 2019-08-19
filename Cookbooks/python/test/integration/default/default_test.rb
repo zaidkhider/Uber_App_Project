44a11e34582bdd473f84999a24cc0673e3211745
Zaid Khider <ZKhider@spartaglobal.com>
2019-08-19 12:21:37 +0100

    added an integration test for the Flask Plugin and also added a few other tests that passed successfully

--- Cookbooks/python/test/integration/default/default_test.rb
+++ Cookbooks/python/test/integration/default/default_test.rb
@@ -5,3 +5,16 @@ end
 describe package('python-pip') do
   it { should be_installed }
 end
+
+describe package('libncurses5-dev') do
+  it { should be_installed}
+end
+
+describe package('libffi-dev') do
+  it { should be_installed }
+end
+
+describe pip('Flask') do
+  it { should be_installed }
+  its('version') { should eq '0.10.1'}
+end
